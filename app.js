2bbd3aeb056df80e8c330058f198d94a7cd65e15
Ayan Khan <programing.ninja0@gmail.com>
2021-12-27 12:33:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-const navslide = () => {
-    const burger = document.querySelector('.burger')
-    const nav = document.querySelector('.navlinks')
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.navlinks');
+    const break1 = document.querySelector('.break1');
+    const break2 = document.querySelector('.break2');
 
     if (burger){
         burger.addEventListener('click',() =>{
-            nav.classList.toggle("ham")
-            burger.classList.toggle('ani')
+            nav.classList.toggle("show");
+            setTimeout(function(){
+                nav.classList.toggle("ham"); //This will be delayed for 1/2 second
+            }, 400);
+            burger.classList.toggle('ani');
+            break1.remove();
+            break2.remove();
         })}
 }
-
-navslide();
+navSlide()
